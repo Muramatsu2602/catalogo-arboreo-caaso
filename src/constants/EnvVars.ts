6ab9458955b300154c87469f38edc4ca30cbe5d5
Arthur Vergaças <arthur.martins@db.tec.br>
2023-07-06 18:34:06 -0300

    feat: add development variables

--- src/constants/EnvVars.ts
+++ src/constants/EnvVars.ts
@@ -4,25 +4,31 @@
 
 /* eslint-disable node/no-process-env */
 
-
 export default {
-  NodeEnv: (process.env.NODE_ENV ?? ''),
-  Port: (process.env.PORT ?? 0),
+  NodeEnv: process.env.NODE_ENV ?? '',
+  Port: process.env.PORT ?? 0,
   CookieProps: {
     Key: 'ExpressGeneratorTs',
-    Secret: (process.env.COOKIE_SECRET ?? ''),
+    Secret: process.env.COOKIE_SECRET ?? '',
     // Casing to match express cookie options
     Options: {
       httpOnly: true,
       signed: true,
-      path: (process.env.COOKIE_PATH ?? ''),
+      path: process.env.COOKIE_PATH ?? '',
       maxAge: Number(process.env.COOKIE_EXP ?? 0),
-      domain: (process.env.COOKIE_DOMAIN ?? ''),
-      secure: (process.env.SECURE_COOKIE === 'true'),
+      domain: process.env.COOKIE_DOMAIN ?? '',
+      secure: process.env.SECURE_COOKIE === 'true',
     },
   },
+  DataBase: {
+    username: process.env.DB_USERNAME ?? '',
+    password: process.env.DB_PASSWORD ?? '',
+    host: process.env.DB_HOST ?? '',
+    port: parseInt(process.env.DB_PORT ?? ''),
+    serviceName: process.env.DB_SERVICE_NAME ?? '',
+  },
   Jwt: {
-    Secret: (process.env.JWT_SECRET ??  ''),
-    Exp: (process.env.COOKIE_EXP ?? ''), // exp at the same time as the cookie
+    Secret: process.env.JWT_SECRET ?? '',
+    Exp: process.env.COOKIE_EXP ?? '', // exp at the same time as the cookie
   },
 } as const;
